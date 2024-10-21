b6404a803d11993ccacd2d94d1c3e9db317fc230
babyru <kailaasa.mailer@gmail.com>
2024-10-21 19:29:58 +0300

    removed mock data

--- constants/index.ts
+++ constants/index.ts
@@ -21,91 +21,91 @@ export const navLinks = [
   // },
 ];
 
-const posts = [
-  {
-    id: "1",
-    title: "Every one is bad in someones life",
-    description:
-      "This post highlights the idea that everyone has their own struggles and imperfections, making them 'bad' in someone's eyes. It encourages empathy and understanding towards others.",
-    image:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(1)-SbUpeqd44CGva363FEG2eVEGBZA8sG.jpg",
-    downloadUrl:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(1)-SbUpeqd44CGva363FEG2eVEGBZA8sG.jpg?download=1",
-    name: "Noah White",
-    likes: 0,
-    links: ["https://example.com/download/post1"],
-    tags: [""],
-  },
-  {
-    id: "2",
-    title: "the lion",
-    description: "change is always possible",
-    image:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(2)-m3cPx24Fp6xeXzdF8ac9O1X9KDwWbc.jpg",
-    downloadUrl:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(2)-m3cPx24Fp6xeXzdF8ac9O1X9KDwWbc.jpg?download=1",
-    name: "Hannah Kim",
-    likes: 0,
-    links: ["https://pin.it/3neNSGLoG"],
-    tags: ["#motivation", "#quotes", "#change", "#always", "#possible"],
-  },
-  {
-    id: "3",
-    title: "choose the right path",
-    description: "What you believe in affects your",
-    image:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(3)-jlnib3pFCtrmbWBi6CbzF6NAiiCYvU.jpg",
-    downloadUrl:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(3)-jlnib3pFCtrmbWBi6CbzF6NAiiCYvU.jpg?download=1",
-    name: "Julian Sanchez",
-    likes: 0,
-    links: ["https://pin.it/9GPBcv0mR"],
-    tags: ["#motivation", "#quotes", "#believe", "#path", "#right"],
-  },
-];
+// const posts = [
+//   {
+//     id: "1",
+//     title: "Every one is bad in someones life",
+//     description:
+//       "This post highlights the idea that everyone has their own struggles and imperfections, making them 'bad' in someone's eyes. It encourages empathy and understanding towards others.",
+//     image:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(1)-SbUpeqd44CGva363FEG2eVEGBZA8sG.jpg",
+//     downloadUrl:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(1)-SbUpeqd44CGva363FEG2eVEGBZA8sG.jpg?download=1",
+//     name: "Noah White",
+//     likes: 0,
+//     links: ["https://example.com/download/post1"],
+//     tags: [""],
+//   },
+//   {
+//     id: "2",
+//     title: "the lion",
+//     description: "change is always possible",
+//     image:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(2)-m3cPx24Fp6xeXzdF8ac9O1X9KDwWbc.jpg",
+//     downloadUrl:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(2)-m3cPx24Fp6xeXzdF8ac9O1X9KDwWbc.jpg?download=1",
+//     name: "Hannah Kim",
+//     likes: 0,
+//     links: ["https://pin.it/3neNSGLoG"],
+//     tags: ["#motivation", "#quotes", "#change", "#always", "#possible"],
+//   },
+//   {
+//     id: "3",
+//     title: "choose the right path",
+//     description: "What you believe in affects your",
+//     image:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(3)-jlnib3pFCtrmbWBi6CbzF6NAiiCYvU.jpg",
+//     downloadUrl:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/post%20(3)-jlnib3pFCtrmbWBi6CbzF6NAiiCYvU.jpg?download=1",
+//     name: "Julian Sanchez",
+//     likes: 0,
+//     links: ["https://pin.it/9GPBcv0mR"],
+//     tags: ["#motivation", "#quotes", "#believe", "#path", "#right"],
+//   },
+// ];
 
-const users = [
-  {
-    id: "1",
-    name: "Noah White",
-    username: "noah_white",
-    bio: "🎵📝 I'm Noah White, a musician and writer. My music explores love, nature, and self-discovery. I've published poetry collections and am working on my debut novel, inspired by my love for the outdoors and passion for storytelling. 📝🎵",
-    avatar:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/avatar%20(1)-F6AydUWgMLULIGqE8xhk5b3KJqv1BG.jpg",
-    banner:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/banner%20(1)-YGudX4LjpzgdQXGn5CI8i468HHHNn0.jpg",
-    followers: ["hannah_kim"],
-    following: ["julian_sanchez"],
-    posts: ["a72e9967-1905-4233-a335-6b8fd38ad683"],
-    saved: [""],
-  },
-  {
-    id: "2",
-    name: "Hannah Kim",
-    username: "hannah_kim",
+// const users = [
+//   {
+//     id: "1",
+//     name: "Noah White",
+//     username: "noah_white",
+//     bio: "🎵📝 I'm Noah White, a musician and writer. My music explores love, nature, and self-discovery. I've published poetry collections and am working on my debut novel, inspired by my love for the outdoors and passion for storytelling. 📝🎵",
+//     avatar:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/avatar%20(1)-F6AydUWgMLULIGqE8xhk5b3KJqv1BG.jpg",
+//     banner:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/banner%20(1)-YGudX4LjpzgdQXGn5CI8i468HHHNn0.jpg",
+//     followers: ["hannah_kim"],
+//     following: ["julian_sanchez"],
+//     posts: ["a72e9967-1905-4233-a335-6b8fd38ad683"],
+//     saved: [""],
+//   },
+//   {
+//     id: "2",
+//     name: "Hannah Kim",
+//     username: "hannah_kim",
 
-    bio: "Traveling foodie exploring flavors and cultures worldwide. Sharing the joy of food and travel.",
-    avatar:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/avatar%20(2)-RpPfcP6QKX42LByeGevjX7L49DPoqG.jpg",
-    banner:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/banner%20(2)-9M7o6EjEB2yjFcXsx6sRA82iAXsPxm.jpg",
-    followers: ["julian_sanchez"],
-    following: ["noah_white"],
-    posts: ["680cd699-89b5-4b6d-bcae-bec988163cc8"],
-    saved: [""],
-  },
-  {
-    id: "3",
-    name: "Julian Sanchez",
-    username: "julian_sanchez",
-    avatar:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/avatar%20(3)-aF8YiDYRT8R4pfGjZWeTAJNXgMbfa2.jpg",
-    banner:
-      "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/banner%20(3)-Tj3FZrseHudRRjXO6RyEzwcqEsjcHl.jpg",
-    bio: "Fitness enthusiast and gamer pushing limits and exploring new worlds. Sharing the journey of fitness and gaming.",
-    followers: ["noah_white"],
-    following: ["hannah_kim"],
-    posts: ["f1829401-a10f-4467-9ff6-b2619ee1db48"],
-    saved: [""],
-  },
-];
+//     bio: "Traveling foodie exploring flavors and cultures worldwide. Sharing the joy of food and travel.",
+//     avatar:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/avatar%20(2)-RpPfcP6QKX42LByeGevjX7L49DPoqG.jpg",
+//     banner:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/banner%20(2)-9M7o6EjEB2yjFcXsx6sRA82iAXsPxm.jpg",
+//     followers: ["julian_sanchez"],
+//     following: ["noah_white"],
+//     posts: ["680cd699-89b5-4b6d-bcae-bec988163cc8"],
+//     saved: [""],
+//   },
+//   {
+//     id: "3",
+//     name: "Julian Sanchez",
+//     username: "julian_sanchez",
+//     avatar:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/avatar%20(3)-aF8YiDYRT8R4pfGjZWeTAJNXgMbfa2.jpg",
+//     banner:
+//       "https://vjkurbrp7v9schki.public.blob.vercel-storage.com/banner%20(3)-Tj3FZrseHudRRjXO6RyEzwcqEsjcHl.jpg",
+//     bio: "Fitness enthusiast and gamer pushing limits and exploring new worlds. Sharing the journey of fitness and gaming.",
+//     followers: ["noah_white"],
+//     following: ["hannah_kim"],
+//     posts: ["f1829401-a10f-4467-9ff6-b2619ee1db48"],
+//     saved: [""],
+//   },
+// ];
